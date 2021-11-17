646f91e1d03245086e86000d8951c9e15fc67be4
denny <denny.chang@invos.com.tw>
2021-11-18 00:51:18 +0800

    init project, add leaflet, styled-components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
+import {useEffect} from "react"
+import L from "leaflet"
+import "leaflet/dist/leaflet.css"
+import { GestureHandling } from "leaflet-gesture-handling";
+import "leaflet-gesture-handling/dist/leaflet-gesture-handling.css";
+import styled from 'styled-components'
 import logo from './logo.svg';
 import './App.css';
 
+const Style = styled.div`
+  div.header {
+    background-color: #fff;
+  }
+
+  div.nav {
+    background-color: #F25C54;
+    color: #fff;
+  }
+
+  div#map {
+   height: calc( 100vh - 120px);
+   
+    div.input {
+      position: absolute;
+      top: 20px;
+      left: 0;
+      right: 0;
+      text-align: center;
+      z-index: 500;
+    }
+  }
+`
+
+
+L.Map.addInitHook("addHandler", "gestureHandling", GestureHandling);
 function App() {
+
+  useEffect(()=>{
+
+    let map = L.map("map",{gestureHandling:true, zoomControl: false}).setView([24.805, 121.09], 16);
+
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: 'pk.eyJ1IjoiZGVubm5ueSIsImEiOiJja3cwc2VuYWUxYjc0Mm9tcWc0YWRxOWx6In0.dYbG-41__b453IpMf5nW2A'
+    }).addTo(map);
+
+    return () => map.remove()
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Style>
+      <div className="header" style={{height:"60px",width:"100%"}}>
+        Bike
+      </div>
+      <div className="nav" style={{height:"60px",width:"100%"}}>
+        Youbike 租借站點
+      </div>
+
+      <div id="map">
+        <div className="input">
+          <input placeholder="search...." />
+        </div>
+      </div>
+    </Style>
   );
 }
 
